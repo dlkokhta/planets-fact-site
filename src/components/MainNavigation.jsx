8aa948da89646e8ffd1bb4e8b498c3326cd020e4
dlkokhta <dl.kokhtashvili@gmail.com>
2023-10-12 14:14:25 +0400

    almost done mobile version

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,90 +1,136 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import PlanetsName from "../pages/PlanetsName";
 
-const MainNavigation = () => {
+const MainNavigation = (props) => {
   const [isVisible, setIsVisible] = useState(false);
 
   const hamburgerIconClickhandler = () => {
     setIsVisible(!isVisible);
   };
-  const mercuryChangeHandler = () => {
+  const mercuryClickHandler = () => {
     setIsVisible(!isVisible);
   };
 
-  const venusChangeHandler = () => {
+  const venusClickHandler = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const earthClickHandler = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const marsClickHandler = () => {
+    setIsVisible(!isVisible);
+  };
+  const jupiterClickHandler = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const saturnClickHandler = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const uranusClickHandler = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const neptunClickHandler = () => {
     setIsVisible(!isVisible);
   };
 
   return (
-    <header className="">
-      <div className=" flex justify-between items-center w-full  p-[18px] pr-[24px] pb-[16px] pl-[24px] ">
-        <h1 className=" font-antonio font-normal text-[#FFFFFF] text-2xl">
-          THE PLANETS
-        </h1>
-        <div onClick={hamburgerIconClickhandler}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="17">
-            <g fill="#FFF" fill-rule="evenodd">
-              <path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z" />
-            </g>
-          </svg>
+    <>
+      <header className="">
+        <div className=" flex justify-between items-center w-full  p-[18px] pr-[24px] pb-[16px] pl-[24px] ">
+          <h1 className=" font-antonio font-normal text-[#FFFFFF] text-2xl">
+            THE PLANETS
+          </h1>
+          <div onClick={hamburgerIconClickhandler}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="17">
+              <g fill="#FFF" fill-rule="evenodd">
+                <path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z" />
+              </g>
+            </svg>
+          </div>
         </div>
-      </div>
-      <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
-      {isVisible && (
-        <div className="p-[44px] pr-[24px] pb-[67px] pl-[24px] ">
-          <ul className="flex flex-col gap-5">
-            <Link
-              className="flex"
-              to="/main/mercury"
-              onClick={mercuryChangeHandler}
-            >
-              <div className=" w-5 h-5 rounded-full bg-[#DEF4FC]"></div>
-              <li className="text-[rgb(255,255,255)]">MERCURY</li>
-            </Link>
-            <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
-            <Link
-              className="flex"
-              to="/main/venus"
-              onClick={venusChangeHandler}
-            >
-              <div className=" w-5 h-5 rounded-full bg-[#F7CC7F]"></div>
-              <li className=" text-[#FFFFFF]">VENUS</li>
-            </Link>
-            <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
-            <Link className=" flex" to="/main/earth">
-              <div className=" w-5 h-5 rounded-full bg-[#545BFE]"></div>
-              <li className="text-[#FFFFFF]">EARTH</li>
-            </Link>
-            <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
-            <Link className="flex" to="/main/mars">
-              <div className=" w-5 h-5 rounded-full bg-[#FF6A45]"></div>
-              <li className="text-[#FFFFFF]">MARS</li>
-            </Link>
-            <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
-            <Link className="flex" to="/main/jupiter">
-              <div className=" w-5 h-5 rounded-full bg-[#ECAD7A]"></div>
-              <li className="text-[#FFFFFF]">JUPITER</li>
-            </Link>
-            <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
-            <Link className="flex" to="/main/saturn">
-              <div className=" w-5 h-5 rounded-full bg-[#FCCB6B]"></div>
-              <li className="text-[#FFFFFF]">SATURN</li>
-            </Link>
-            <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
+        <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
+        {isVisible && (
+          <div className="p-[44px] pr-[24px] pb-[67px] pl-[24px] ">
+            <ul className="flex flex-col gap-5">
+              <Link
+                className="flex"
+                to="/main/mercury"
+                onClick={mercuryClickHandler}
+              >
+                <div className=" w-5 h-5 rounded-full bg-[#DEF4FC]"></div>
+                <li className="text-[rgb(255,255,255)]">MERCURY</li>
+              </Link>
+              <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
+              <Link
+                className="flex"
+                to="/main/venus"
+                onClick={venusClickHandler}
+              >
+                <div className=" w-5 h-5 rounded-full bg-[#F7CC7F]"></div>
+                <li className=" text-[#FFFFFF]">VENUS</li>
+              </Link>
+              <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
+              <Link
+                className=" flex"
+                to="/main/earth"
+                onClick={earthClickHandler}
+              >
+                <div className=" w-5 h-5 rounded-full bg-[#545BFE]"></div>
+                <li className="text-[#FFFFFF]">EARTH</li>
+              </Link>
+              <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
+              <Link className="flex" to="/main/mars" onClick={marsClickHandler}>
+                <div className=" w-5 h-5 rounded-full bg-[#FF6A45]"></div>
+                <li className="text-[#FFFFFF]">MARS</li>
+              </Link>
+              <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
+              <Link
+                className="flex"
+                to="/main/jupiter"
+                onClick={jupiterClickHandler}
+              >
+                <div className=" w-5 h-5 rounded-full bg-[#ECAD7A]"></div>
+                <li className="text-[#FFFFFF]">JUPITER</li>
+              </Link>
+              <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
+              <Link
+                className="flex"
+                to="/main/saturn"
+                onClick={saturnClickHandler}
+              >
+                <div className=" w-5 h-5 rounded-full bg-[#FCCB6B]"></div>
+                <li className="text-[#FFFFFF]">SATURN</li>
+              </Link>
+              <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
 
-            <Link className="flex" to="/main/dimitri">
-              <div className=" w-5 h-5 rounded-full bg-[#65F0D5]"></div>
-              <li className="text-[#FFFFFF]">URANUS</li>
-            </Link>
-            <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
-            <Link className="flex" to="/main/neptune">
-              <div className=" w-5 h-5 rounded-full bg-[#497EFA]"></div>
-              <li className="text-[#FFFFFF]">NEPTUNE</li>
-            </Link>
-          </ul>
-        </div>
-      )}
-    </header>
+              <Link
+                className="flex"
+                to="/main/uranus"
+                onClick={uranusClickHandler}
+              >
+                <div className=" w-5 h-5 rounded-full bg-[#65F0D5]"></div>
+                <li className="text-[#FFFFFF]">URANUS</li>
+              </Link>
+              <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
+              <Link
+                className="flex"
+                to="/main/neptune"
+                onClick={neptunClickHandler}
+              >
+                <div className=" w-5 h-5 rounded-full bg-[#497EFA]"></div>
+                <li className="text-[#FFFFFF]">NEPTUNE</li>
+              </Link>
+            </ul>
+          </div>
+        )}
+      </header>
+    </>
   );
 };
 
--- src/pages/PlanetsName.jsx
+++ src/pages/PlanetsName.jsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom";
 import planetsData from "../data/data.json";
+import React from "react";
 
-const PlanetsName = () => {
+function PlanetsName() {
   const { name } = useParams();
 
   console.log(name);
@@ -9,24 +10,91 @@ const PlanetsName = () => {
   const planetData = planetsData.find(
     (item) => item.name.toUpperCase() === name.toUpperCase()
   );
-  console.log(planetData);
 
   return (
     <>
-      <div className=" text-white p-[44px] pr-[24px] pb-[67px] pl-[24px]">
-        <div className="flex justify-between">
-          <h1>OVERVIEW</h1>
-          <h1>STRUCTURE</h1>
-          <h1>SURFACE</h1>
-        </div>
+      <div className="">
         <div>
-          <h1>{planetData.name}</h1>
-          <h1>{planetData.overview.content}</h1>
-          <a className="w-full">{planetData.overview.source}</a>
+          <div className="flex justify-between items-center p-[17px] pr-[24px] pl-[24px]">
+            <h1 className="text-white text-[9px] font-spartan font-bold">
+              OVERVIEW
+            </h1>
+            <h1 className="text-white text-[9px] font-spartan font-bold">
+              STRUCTURE
+            </h1>
+            <h1 className="text-white text-[9px] font-spartan font-bold">
+              SURFACE
+            </h1>
+          </div>
+
+          <div className=" h-[1px] bg-white bg-opacity-20 w-[100%] flex "></div>
+
+          <img
+            className=" mx-auto w-[111px] mt-[95px]"
+            src={planetData.images.planet}
+          />
+
+          <div className=" text-white mt-[95px] mb-[28px] pr-[24px]  pl-[24px]">
+            <h1 className=" font-antonio text-center text-[40px] pt-[0]">
+              {planetData.name}
+            </h1>
+
+            <p className="">{planetData.overview.content}</p>
+
+            <div className=" flex justify-center items-center gap-[4px] ">
+              <a href="{planetData.overview.source}">
+                Source : <u>Wikipedia</u>
+              </a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12">
+                <path
+                  fill="#FFF"
+                  d="M11.34.66C10.9.22 10.37 0 9.75 0h-7.5C1.63 0 1.1.22.66.66.22 1.1 0 1.63 0 2.25v7.5c0 .62.22 1.15.66 1.59.44.44.97.66 1.59.66h7.5c.62 0 1.15-.22 1.59-.66.44-.44.66-.97.66-1.59v-7.5c0-.62-.22-1.15-.66-1.59zM10 6.25a.467.467 0 01-.305.46.544.544 0 01-.195.04.465.465 0 01-.352-.149L8.023 5.476 3.852 9.648a.481.481 0 01-.352.149.48.48 0 01-.352-.149l-.796-.797a.48.48 0 01-.149-.351.48.48 0 01.149-.352l4.172-4.172-1.125-1.125c-.162-.15-.198-.333-.11-.546A.467.467 0 015.75 2H9.5c.135 0 .253.05.352.148A.48.48 0 0110 2.5v3.75z"
+                  opacity=".5"
+                />
+              </svg>
+            </div>
+          </div>
         </div>
-        <div></div>
+
+        <div className="pr-[24px] pl-[24px] flex flex-col gap-[8px]">
+          <div
+            className=" flex justify-between items-center border-solid border-[1px] border-white 
+            p-[9px] pr-[24px] pb-[13px] pl-[24px]"
+          >
+            <div className=" font-spartan text-[8px] font-bold text-white">
+              ROTATION TIME
+            </div>
+            <div className=" font-antonio text-[20px] font-normal text-white">
+              {planetData.rotation.toUpperCase()}
+            </div>
+          </div>
+          <div className=" flex justify-between items-center  border-solid border-[1px] border-white  p-[9px] pr-[24px] pb-[13px] pl-[24px]">
+            <div className="font-spartan text-[8px] font-bold text-white">
+              REVOLUTION TIME
+            </div>
+            <div className=" font-antonio text-[20px] font-normal text-white">
+              {planetData.revolution}
+            </div>
+          </div>
+          <div className="flex justify-between items-center border-solid border-[1px] border-white  p-[9px] pr-[24px] pb-[13px] pl-[24px]">
+            <div className="font-spartan text-[8px] font-bold text-white">
+              RADIUS
+            </div>
+            <div className=" font-antonio text-[20px] font-normal text-white">
+              {planetData.radius}
+            </div>
+          </div>
+          <div className=" flex justify-between items-center border-solid border-[1px] border-white  p-[9px] pr-[24px] pb-[13px] pl-[24px]">
+            <div className="font-spartan text-[8px] font-bold text-white">
+              AVERAGE TEMP
+            </div>
+            <div className=" font-antonio text-[20px] font-normal text-white">
+              {planetData.temperature}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
-};
+}
 export default PlanetsName;
